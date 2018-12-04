c178ed3c6ae9bbddf23eaff71cc45604ce89bf15
20dzhong <20dzhong@gmail.com>
2018-12-04 14:41:04 -0800

    added constructor in trajectory iterator i think

--- src/main/java/frc/team852/lib/purepursuit/TrajectoryIterator.java
+++ src/main/java/frc/team852/lib/purepursuit/TrajectoryIterator.java
@@ -1,7 +1,10 @@
 package frc.team852.lib.purepursuit;
 
+import frc.team852.lib.utilities.Rotation2D;
+
 public class TrajectoryIterator {
-    private final double x0, x1, y0, y1, curve0, curve1, orient0, orient1;
+    private final double x0, x1, y0, y1, curve0, curve1, rot0, rot1;
+    private final Rotation2D rot0, rot1;
 
     TrajectoryIterator(TrajectoryPosition p0, TrajectoryPosition p1) {
         x0 = p0.getTranslation().getX();
@@ -10,7 +13,13 @@ public class TrajectoryIterator {
         y0 = p0.getTranslation().getY();
         y1 = p1.getTranslation().getY();
 
-        curve0 =
+        curve0 = p0.getCurvature();
+        curve1 = p1.getCurvature();
+
+        rot0 = p0.getRotation();
+        rot1 = p1.getRotation();
+
+
 
 
 
