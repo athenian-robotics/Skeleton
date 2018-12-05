c794902ae960458f0432f10c92ae90d4b1376b8c
20dzhong <20dzhong@gmail.com>
2018-12-05 13:52:39 -0800

    Added JSON dumper

--- src/main/java/frc/team852/lib/utilities/JSONEditor.java
+++ src/main/java/frc/team852/lib/utilities/JSONEditor.java
@@ -13,7 +13,6 @@ public final class JSONEditor {
     private static String relativePath; // relative path
     private static String absPath; // absolute path
 
-    public IPose2D pose = null;
 
     static {
         // set default static file name as test, so it no break and save it to something
@@ -23,55 +22,35 @@ public final class JSONEditor {
         absPath = systemPath + filename;
     }
 
-    public static boolean dumpJSON() {
+
+
+    public static void dumpJSON(JSONWritable jw) throws IOException {
         // TODO Donovan implement JSON
-        JSONArray jPose = new JSONArray();
-
-        return Boolean.parseBoolean(null);
+        // if (!checkFile(filename)) return false;
+        FileWriter file = new FileWriter(absPath);
+        file.write(jw.toJSON());
     }
 
-    public static void clear() {
-
+    public static void setName(String name) {
+        filename = name;
+        relativePath = workingPath + filename;
+        absPath = systemPath + filename;
     }
 
-
-    public static boolean setName(String name) {
-        if (checkFile(name)) return false;
-        else {
-            filename = name;
-            relativePath = workingPath + filename;
-            absPath = systemPath + filename;
-        }
-        return true;
-    }
-
-    public static boolean checkFile(String name) {
-        return (new File (systemPath + name).exists());
-    }
-
-    class Getter {
-        public String filename() {
+    public String filename() {
             return filename;
-        }
-
-        public String absolutePath() {
-            return absPath;
-        }
-
-        public IPose2D prevPose() {
-            return pose;
-        }
     }
 
+    public String absolutePath() {
+            return absPath;
+    }
 
     // TESTING WILL DELETE
-    public static void main(String args[]) {
-        JSONEditor.setName("path_one.json");
-        Getter getter = new JSONEditor().new Getter();
-        System.out.println(getter.filename());
+    public static void main(String args[]) throws IOException {
+        //JSONEditor.setName(");
         Pose2D ok = new Pose2D();
-        Pose2DWithCurvature ok1 = new Pose2DWithCurvature(1, 2,3);
-        JSONEditor.dumpJSON(ok1);
+        JSONEditor.dumpJSON(ok);
+        // Pose2DWithCurvature ok1 = new Pose2DWithCurvature(1, 2,3);
         //System.out.println(JSONDump.getFilename());
         //System.out.println(JSONDump.getAbsolutePath());
     }
