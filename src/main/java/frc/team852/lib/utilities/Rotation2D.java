571a4fde9b7713cf4ed5e4e2fec3e292162eab34
Turtle1331 <turtle1331@users.github.com>
2018-12-17 08:45:51 -0800

    Add Rotation2D manipulation methods, finish interpolate

--- src/main/java/frc/team852/lib/utilities/Rotation2D.java
+++ src/main/java/frc/team852/lib/utilities/Rotation2D.java
@@ -32,17 +32,35 @@ public class Rotation2D implements IRotation2D {
         return sin;
     }
 
+    public double getAngle() {
+        return Math.atan2(sin, cos);
+    }
+
+    public double angleTo(IRotation2D other) {
+        Rotation2D o = other.getRotation().inverse();
+        return inverse().rotate(o).getAngle();
+    }
+
     public Rotation2D inverse() {
         return new Rotation2D(cos, -sin);
     }
 
-    public Rotation2D interpolate(IRotation2D other, double t) {
-        // TODO please finish
+    public Rotation2D rotate(IRotation2D other) {
         Rotation2D o = other.getRotation().inverse();
-        return new Rotation2D(
-//                cos * o.cos - sin * o.sin,
-//                blah blah blah
-        );
+        return new Rotation2D(cos * o.cos - sin * o.sin, cos * o.sin + sin * o.cos);
+    }
+
+    public Rotation2D rotate(double a) {
+        return rotate(new Rotation2D(a));
+    }
+
+    public Rotation2D scale(double s) {
+        return new Rotation2D(s * getAngle());
+    }
+
+    public Rotation2D interpolate(IRotation2D other, double t) {
+        Rotation2D o = other.getRotation().inverse();
+        return rotate(t * angleTo(o));
     }
 
     @Override
